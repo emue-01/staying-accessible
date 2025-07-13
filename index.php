892494e0a6e8dd1f4a8146504ff9d6315244d58c
emue <emilie@emue-design.fr>
2025-07-13 17:25:26 +0200

    getting translations from yaml files

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
+@include_once __DIR__ . '/vendor/autoload.php';
 use Kirby\Cms\App as Kirby;
 Kirby::plugin('emue/staying-accessible', [
     'blueprints' => [
         'tabs/accessibility' => __DIR__ . '/blueprints/accessibility.yml'
     ],
-    'translations' => [
-        'fr' => [
-
-        ]
-    ]
+    // borrowed from tobimori/kirby-seo
+    'translations' => A::keyBy(A::map(
+		Dir::read(__DIR__ . '/translations'),
+		fn ($file) => A::merge([
+			'lang' => F::name($file),
+		], Yaml::decode(F::read(__DIR__ . '/translations/' . $file)))
+	), 'lang'),
 ]);
